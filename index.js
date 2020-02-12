c7c86962cb87b96d0aaee523b37cfcf2a1b33e84
RazTazPaz <niklas.kaluza@gmx.de>
2020-02-12 14:06:29 +0100

    h

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 	if (command === 'delete')	  {
-	 if(message.channel === client.channels.find("name", "altis-online")){
+	 if(message.channel === client.channels.find("name", "exile_altis_online")){
 		if (message.member.roles.find("name", "Admin")){
 			
 		const fetched = 10;
